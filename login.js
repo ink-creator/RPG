8a5b927beb04d9a087c15dceeec14bc0cb89768f
ink-creator <gugasksk@gmail.com>
2025-12-25 22:46:01 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,20 @@
+const contas = {
+  Ravir: { senha: "furryto", pagina: "players/p1.html" },
+  Ruan: { senha: "pato", pagina: "players/p2.html" },
+  Luma: { senha: "monitora", pagina: "players/p3.html" },
+  Cayo: { senha: "cabeludo", pagina: "players/p4.html" },
+  Juliana: { senha: "ruiva", pagina: "players/p5.html" },
+  mestre:  { senha: "mestre123", pagina: "mestre/mestre.html" }
+};
+
+function login() {
+  const user = document.getElementById("usuario").value;
+  const pass = document.getElementById("senha").value;
+
+  if (contas[user] && contas[user].senha === pass) {
+    localStorage.setItem("logado", user);
+    window.location.href = contas[user].pagina;
+  } else {
+    document.getElementById("erro").innerText = "Usuário ou senha incorretos";
+  }
+}
