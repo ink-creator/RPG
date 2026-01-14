b896bd4d1f9dd7ba8d7ccc40e5f349c94fd88382
ink-creator <gugasksk@gmail.com>
2026-01-14 12:01:41 -0300

    Update players.js

--- players.js
+++ players.js
@@ -1,12 +1,2 @@
 // player.js
-export function getPlayerId() {
-  const body = document.body;
-  const id = body?.dataset?.player;
-
-  if (!id) {
-    console.warn("data-player não definido no <body>, usando debug-player");
-    return "debug-player";
-  }
-
-  return id;
-}
+export const PLAYER_ID = document.body.dataset.player || "debug-player";
