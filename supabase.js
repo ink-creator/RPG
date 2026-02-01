d780602e5395ab19f3f137d3a02109331abda137
ink-creator <gugasksk@gmail.com>
2026-02-01 10:03:52 -0300

    Change Supabase client key to a JWT token
    Updated Supabase client initialization with a new key.

--- supabase.js
+++ supabase.js
@@ -3,5 +3,5 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 export const supabase = createClient(
   "https://zhfqewgnnfejfyfkmyae.supabase.co",
-  "sb_publishable_DGpfjOL9IQ4t8DEiz06fhQ_YDnJABI6"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpoZnFld2dubmZlamZ5ZmtteWFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4OTU2MTQsImV4cCI6MjA4NTQ3MTYxNH0.j1l_qCI88GyK_5zLu-3JYyeyNFBJKHEkxyY_kDhdI1k"
 );
