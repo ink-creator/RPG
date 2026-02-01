14fd9bf04a06e469ca8e42f853b4eb3e491fbd54
ink-creator <gugasksk@gmail.com>
2026-02-01 10:48:48 -0300

    Update Supabase configuration in mestre.js
    Updated Supabase URL and key for the client.

--- mestre/mestre.js
+++ mestre/mestre.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const supabaseUrl = "SUA_URL_AQUI";
-const supabaseKey = "SUA_ANON_KEY_AQUI";
+const supabaseUrl = "https://zhfqewgnnfejfyfkmyae.supabase.co";
+const supabaseKey = "sb_publishable_DGpfjOL9IQ4t8DEiz06fhQ_YDnJABI6";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
